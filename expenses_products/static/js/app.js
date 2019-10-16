8ab2f2b1be714a766271a322ebc127d8f17ecc94
Juju-And <justyna.andrejczuk@gmail.com>
2019-10-16 23:08:22 +0000

    jquery working

--- expenses_products/static/js/app.js
+++ expenses_products/static/js/app.js
@@ -1,18 +1,3 @@
 console.log("moja DUPA");
 
 $('#content').text("dupa")
-
-
-//$.ajax({
-//    type: 'GET',
-//    url: 'http://example/functions.php',
-//    data: { get_param: 'value' },
-//    dataType: 'json',
-//    success: function (data) {
-//        $.each(data, function(index, element) {
-//            $('body').append($('<div>', {
-//                text: element.name
-//            }));
-//        });
-//    }
-//});
